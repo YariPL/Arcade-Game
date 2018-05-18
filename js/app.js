ad98c4f5eaad41da522fdd47dd832cf4969edf5f
Yaroslav <yaroslav.khudchenko@gmail.com>
2018-05-18 23:12:42 +0200

    draw enemies pictures and add collision
    -draw enemies pictures
    -when player touch enemy reset player position and -1 live
    -add hat to player icon :)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 // Enemies our player must avoid
-let enemy = class Enemy{
+class Enemy{
    constractor() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
     }
 
 // Update the enemy's position, required method for game
@@ -20,12 +19,15 @@ let enemy = class Enemy{
 
 // Draw the enemy on the screen, required method for game
     static render() {
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.drawImage(Resources.get(this.sprite), this.x-100, this.y+90);
+        ctx.drawImage(Resources.get(this.sprite), this.x-200, this.y+175);
+
     };
 }
-enemy.x = 300;
-enemy.y = 300;
-// Now write your own player class
+Enemy.x = 200;
+Enemy.y = 60;
+Enemy.sprite = 'images/enemy-bug.png';
 
 
 
@@ -33,7 +35,7 @@ enemy.y = 300;
 
 
 // Place all enemy objects in an array called allEnemies
-let allEnemies = [];
+let allEnemies = [Enemy];
 
 // Place the player object in a variable called player
 class player {
@@ -41,41 +43,64 @@ class player {
     constractor(){
     }
     static update(){}
+    //draw the player icon-avatar
     static render(){
-        ctx.drawImage(Resources.get('images/char-boy.png'), this.x, this.y);
+        ctx.drawImage(Resources.get('images/char-boy1.png'), this.x, this.y);
     }
 
-    static handleInput(e, x, y){
-        //if 37 move left so on
+    static handleInput(e){
         console.log('keyup');
-
+        //moving (if left go left and so on)
         switch(e) {
             case 'left':
-                console.log('1ass');//move left
-                player.x=player.x-100;
+                console.log('left');//move left
+                if(player.x > 26){
+                    player.x=player.x-100;
+                }
                 break;
             case 'up':
-                console.log('2ass');//move up
-                player.y=player.y-85;
+                console.log('up');//move up
+                if(player.y >= 25){
+                    player.y=player.y-85;
+                }
                 break;
             case 'right':
-                console.log('3ass');//move right
-                ;
-                player.x=player.x+100;
-                break
+                console.log('right');//move right
+                if(player.x < 399){
+                    player.x=player.x+100;
+                }
+                break;
             case 'down':
-                console.log('4ass');//move down
-                ;
-                player.y=player.y+85;
-                break 
+                console.log('down');//move down
+                if(player.y < 399){
+                    player.y=player.y+85;
+                }
+                break ;
+        }
+        //if collision happend
+        if(player.x === Enemy.x && player.y === Enemy.y){
+            console.log('banda');
+            //reset to a starting point
+            player.x = 200;
+            player.y = 400;
+            //minus 1 live
+            player.lives--;
+            //if player loose all 3 live happyend is impossible
+            if (player.lives === 0) {
+                alert('hana bratwa');
+            }
         }
     }
 }
 
 player.x = 200;
 player.y = 400;
+player.lives = 3;
+player.points = 0;
 
-
+if(player.x === Enemy.x && player.y === Enemy.y){
+    console.log('banda');
+}
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
